9b9a54d42c942d68368701f997abfcb947ee88c7
Esmée Zootjes <esmeezootjes@gmail.com>
2023-12-06 15:30:15 +0100

    done

---
+++ src/components/resume/ResumeOverview.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import Contact from "./Contact";
+import Profile from "./Profile";
+import Skills from "./Skills";
+import Experience from "./Experience";
+
+function ResumeOverview() {
+  return (
+    <div>
+      <Header />
+      <Contact />
+      <Profile />
+      <Skills />
+      <Experience />
+      <Experience />
+      <Skills />
+    </div>
+  );
+}
+
+export default ResumeOverview;
