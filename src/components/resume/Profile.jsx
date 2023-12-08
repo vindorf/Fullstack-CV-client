ac6d014899df8af056b90c73d6c39d614b0dd484
Esmée Zootjes <esmeezootjes@gmail.com>
2023-12-08 18:54:43 +0100

    WIP: b25dc92 Merge pull request #9 from vindorf/new-components

--- src/components/resume/Profile.jsx
+++ src/components/resume/Profile.jsx
@@ -1,64 +1,20 @@
+import { useState } from "react";
 import React from "react";
 
-import { useState } from "react";
-
 function Profile() {
-  const [fields, setFields] = useState([{ value: "", isVisible: true }]); // State to manage form fields
-
-  const [hideForm, sethideForm] = useState("show");
-  const [showResumeResult, setShowResumeResult] = useState("hide");
-  const [removedFields, setRemovedFields] = useState([]); // State to store removed fields
-
-  // Function to handle adding new fields
-  const handleAddField = () => {
-    const newFields = [...fields, { value: "", isVisible: true }];
-    setFields(newFields); // Update the state with the new array of fields
-  };
-
-  const handleRemoveField = (index) => {
-    const updatedFields = fields.filter((field, i) => i !== index); // Remove the field at the specified index
-    setFields(updatedFields); // Update the state with the updated array of fields
-  };
-
-  // Function to handle input change in each field
-  const handleFieldChange = (index, event) => {
-    const newFields = [...fields]; // Create a copy of the fields array
-    newFields[index].value = event.target.value; // Update the value of the specific field
-    setFields(newFields); // Update the state with the new array of fields
-  };
-
-  // Function to handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    sethideForm("hide");
-    setShowResumeResult("show");
-
-    console.log("Form submitted:", fields);
-  };
+  const [profile, setProfile] = useState("");
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        {fields.map((field, index) => (
-          <div key={index}>
-            {field.isVisible && (
-              <div className={`${hideForm}`}>
-                <input
-                  type="text"
-                  value={field.value}
-                  onChange={(e) => handleFieldChange(index, e)} // Handle input change for each field
-                />
-                <button type="button" onClick={handleAddField}>
-                  +
-                </button>
-                <button type="button" onClick={() => handleRemoveField(index)}>
-                  -
-                </button>
-              </div>
-            )}
-          </div>
-        ))}
+      <h3>PROFILE</h3>
+      <form>
+        <input
+          type="text"
+          name="profile"
+          placeholder="Introduce yourself!"
+          value={props.firstName}
+          onChange={handleFirstNameChange}
+        />
       </form>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+import Profile from "../components/resume/Profile";
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Profile />
     </div>
   );
 }
