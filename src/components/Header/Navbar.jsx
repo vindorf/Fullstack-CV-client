d7774ceae5b5592e48e04f7d606f8885fabdc590
Sena Aydin <senaaydin484@gmail.com>
2023-12-09 17:21:18 +0100

    added nav style

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,51 +2,37 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/auth.context";
-import brandLogo from "../../assets/ironhack.webp";
 import "./Navbar.css";
 
 function Navbar() {
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const { isLoggedIn, logOutUser } = useContext(AuthContext);
 
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/">
-        <img className="brandlogo-img" src={brandLogo} alt="Logo" />
-      </Link>
+      <div className="navbar-nav navbar-right">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
 
-      <div
-        className="collapse navbar-collapse justify-content-end"
-        id="navbarNav"
-      >
-        <div className="navbar-nav">
-          <Link className="nav-link" to="/">
-            Home
-          </Link>
-
-          {isLoggedIn ? (
+        {isLoggedIn ? (
+          <>
             <Link className="nav-link" to="/resumes">
               My Resumés
             </Link>
-          ) : (
-            <>
-              <Link
-                className="nav-link btn btn-outline-light mr-2"
-                to="/signup"
-              >
-                Sign Up
-              </Link>
-              <Link className="nav-link btn btn-outline-light" to="/login">
-                Login
-              </Link>
-            </>
-          )}
-
-          {isLoggedIn && (
-            <span className="navbar-text">
-              {user && user.email} is logged in!
-            </span>
-          )}
-        </div>
+            <Link className="nav-link" onClick={logOutUser}>
+              Logout
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link className="nav-link" to="/signup">
+              Sign Up
+            </Link>
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
